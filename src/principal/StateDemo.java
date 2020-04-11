fca3039429e06918419a7a140278f576f6f2c632
beaapaixao <brendacefet@gmail.com>
2020-04-11 02:21:49 -0300

    Implementação da StateDemo

--- src/principal/StateDemo.java
+++ src/principal/StateDemo.java
@@ -1,6 +1,10 @@
 package principal;
 
 import automatos.states.ConcreteStateA_Novo;
+import automatos.states.ConcreteStateB_Aprovado;
+import automatos.states.ConcreteStateC_EmTransporte;
+import automatos.states.ConcreteStateD_Entregue;
+import automatos.states.ConcreteStateF_Finalizado;
 import automatos.states.ContextPedido;
 
 public class StateDemo {
@@ -14,7 +18,17 @@ public class StateDemo {
 		  novo.criarPedido(pedido);
 		
 		
-		
+		  ConcreteStateB_Aprovado aprovado = new ConcreteStateB_Aprovado(pedido);
+		  aprovado.aprovarPedido(pedido);
+		  
+		  ConcreteStateC_EmTransporte transportando = new ConcreteStateC_EmTransporte(pedido);
+		  transportando.transportarPedido(pedido);
+		  
+		  ConcreteStateD_Entregue entregue = new ConcreteStateD_Entregue(pedido);
+		  entregue.entregarPedido(pedido);
+		  
+		  ConcreteStateF_Finalizado finalizar = new ConcreteStateF_Finalizado(pedido);
+		  finalizar.finalizarPedido(pedido);
 	}
 
 }
